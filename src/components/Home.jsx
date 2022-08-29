0a4e4808c57fc7bace96afde0ffc40d81aa97493
Andrés Garbarz <67331050+andresgarbarz@users.noreply.github.com>
2022-08-29 16:11:27 -0300

    Halpe in process
    .

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../css/Home.css'
+
+function Home() {
+
+  return (
+    <div className="container">
+    <div className="full-image"></div>
+    <div className="images"></div>
+    </div>
+  )
+}
+
+export default Home
