087251db142966eeef7339ac9df5c2c0636a154c
Andrés Garbarz <67331050+andresgarbarz@users.noreply.github.com>
2022-10-11 01:48:09 -0300

    Home + Sections Merge
    .

--- src/components/Sections.jsx
+++ src/components/Sections.jsx
@@ -6,14 +6,33 @@ function Sections() {
         <>
         <div className='container'>
             <div className='section' id='student'>
+                <div className='txtbox' id='stxtbox'>
+                    <h1 className='stxt'>Student</h1>
+                </div>
                 <img src="./src/assets/home/student.jpg" alt=""/>
             </div>
             <div className='section' id='athlete'>
+                <div className='txtbox' id='atxtbox'>
+                    <h1 className='stxt'>Athlete</h1>
+                </div>
                 <img src="./src/assets/home/athlete.jpg" alt=""/>
             </div>
-            <div className='section' id='developer'></div>
+            <div className='section' id='developer'>
+                <div className='txtbox' id='dtxtbox'>
+                    <h1 className='stxt'>Developer</h1>
+                </div>
+                <img src="./src/assets/home/developer.jpg" alt=""/>
+            </div>
+            <div className='section' id='musician'>
+                <div className='txtbox' id='mtxtbox'>
+                    <h1 className='stxt'>Musician</h1>
+                </div>
+                <img src="./src/assets/home/musician.jpg" alt=""/>
+            </div>
             <div className='section' id='musician'></div>
-            <div className='extra' id='andmore'></div>
+            <div className='extra' id='andmore'>
+                <img src="./src/assets/home/extra.jpg" alt=""/>
+            </div>
         </div>
         </>
     )
