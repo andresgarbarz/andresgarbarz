5a6adaf4021bd51011f687c152b7472e83147ddb
Andrés Garbarz <67331050+andresgarbarz@users.noreply.github.com>
2023-02-18 13:09:00 -0300

    Migration sweet alert
    .

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Home from '../Content/LandingPage/Home'
 import Sections from '../Content/LandingPage/Sections'
+import Swal from 'sweetalert2'
 
 function Landing() {
+    window.onload = () => {Swal.fire({titleText: "Andrés Garbarz says", text: "This website is being migrated to next.js for SEO and optimization purposes, therefore there is no visible progress. However, you can still navigate since most sections are almost complete.", icon: "info", confirmButtonText: "Got it!", confirmButtonColor: "#4EABFF"})}
     return (
         <>
             <Home></Home>
